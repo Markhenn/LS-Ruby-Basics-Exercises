cf817a2881c6e562e17a5b5e2893bcdedc4559bc
Mark Henninger <markhenningerwork@gmail.com>
2019-02-04 16:07:30 +0100

    Add methods execises

---
+++ Methods/5.rb
@@ -0,0 +1,7 @@
+# Using the following code, write a method called car that takes two arguments and prints a string containing the values of both arguments.
+
+def car(make, type)
+  puts "#{make} #{type}"
+end
+
+car('Toyota', 'Corolla')
